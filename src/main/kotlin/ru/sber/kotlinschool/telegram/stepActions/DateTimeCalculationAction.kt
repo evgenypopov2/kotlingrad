8cbc1ccb9506c7007f3076afb39030b63dd1c806
zainulina.guzal <guzal.zainulina@gmail.com>
2023-02-15 21:50:10 +0300

    Телеграм-2: Доработка отображения времени работы

--- src/main/kotlin/ru/sber/kotlinschool/telegram/stepActions/DateTimeCalculationAction.kt
+++ src/main/kotlin/ru/sber/kotlinschool/telegram/stepActions/DateTimeCalculationAction.kt
@@ -1,5 +1,6 @@
 package ru.sber.kotlinschool.telegram.stepActions
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup
@@ -9,25 +10,37 @@ import ru.sber.kotlinschool.telegram.entity.Step
 @Component("DATE_TIME_CALC")
 class DateTimeCalculationAction : Action() {
 
+    @Value("\${admin.startWorkinHour}")
+    private val  start: String = ""
+
+    @Value("\${admin.endWorkingHour}")
+    private val  end: String = ""
+
     override fun execute(currentStep: Step, chatId: String): SendMessage {
         val responseMessage = SendMessage(chatId, currentStep.messageForUser)
         responseMessage.enableMarkdown(true)
 
         val range = if (currentStep.configParams.isNotEmpty()) toRange(currentStep.configParams[0])
-        else toRange("9-18")
+        else start.toInt()..end.toInt()
 
         val inlineKeyboardMarkup = InlineKeyboardMarkup() //Создаем объект разметки клавиатуры
         val keyboardButtonsRow1: MutableList<InlineKeyboardButton> = ArrayList()
+        val keyboardButtonsRow2: MutableList<InlineKeyboardButton> = ArrayList()
         val rowList: MutableList<List<InlineKeyboardButton>> = ArrayList() //Создаём ряд
-
+        val middleNumber = range.first + (range.last-range.first)/2
         for (i in range) {
             val inlineKeyboardButton1 = InlineKeyboardButton()
             inlineKeyboardButton1.text = "${i}:00"
             inlineKeyboardButton1.callbackData = "${i}:00"
-            keyboardButtonsRow1.add(inlineKeyboardButton1)
+            if(i <= middleNumber) {
+                keyboardButtonsRow1.add(inlineKeyboardButton1)
+            } else {
+                keyboardButtonsRow2.add(inlineKeyboardButton1)
+            }
         }
 
         rowList.add(keyboardButtonsRow1)
+        rowList.add(keyboardButtonsRow2)
 
         inlineKeyboardMarkup.keyboard = rowList
 
